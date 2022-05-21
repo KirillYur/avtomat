15db14e2877201f053b61542aae50f6c6fa01880
KirillYur <72611465+KirillYur@users.noreply.github.com>
2022-05-21 14:58:48 +0300

    Update code.py

--- code.py
+++ code.py
@@ -294,6 +294,7 @@ st.write("""# Вибрационная диаграмма лопаточного
 
 x1 = np.linspace(0, 50, 50)
 y1 = (fstA0**2+B*x1**2)**0.5
+y2 = (fstA1**2+B*n**2)**0.5
 fig, ax = plt.subplots()
-ax.plot(x1, y1)
+ax.plot(x1, y1, y2)
 st.pyplot(fig)
